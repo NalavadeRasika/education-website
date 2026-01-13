eab7256e981430da27d2f62263f5c48a1fbbb6dd
NalavadeRasika <rasikanalavade41@gmail.com>
2026-01-13 14:36:32 +0530

    Initial website commit

---
+++ src/components/allcourses/CoursesCard.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import "./courses.css"
+import { coursesCard } from '../../dummydata'
+
+const CoursesCard = () => {
+  return (
+    <>
+     <section  className='coursesCard'>
+        <div className="container grid2">
+            {coursesCard.map((val)=>{
+               return ( 
+                <div className='items'>
+                    <div className='content flex'>
+                        <div className='left'>
+                            <div className='img'>
+                                <img src={val.cover} alt='course '  />
+                            </div>
+                        </div>
+                       <div className='text'>
+                        <h1>{val.coursesName}</h1>
+                        <div className='rate'>
+                            <i className='fa fa-star'></i>
+                            <i className='fa fa-star'></i>
+                            <i className='fa fa-star'></i>
+                            <i className='fa fa-star'></i>
+                            <i className='fa fa-star'></i>
+                            <label htmlFor="">(5.0)</label>
+                        </div>
+                        <div className='details'>
+                            {/* //this beacuse we are fetch the nested api
+                            val come from courTeacher is which dat au want to fetch  */}
+                            {val.courTeacher.map((details)=>(
+                                <>
+                                <div className='box'>
+                                    <div className='dimg'>
+                                        <img src={details.dcover} alt='' />
+                                    </div>
+                                    <div className='para'>
+                                        <h4>{details.name}</h4>
+                                    </div>
+                                </div>
+                                <span>{details.totalTime}</span>
+                                </>
+            ))}
+                        </div>
+                       </div>
+                    </div>
+                </div>
+                 )
+           })}
+        </div>
+     </section>
+    </>
+  )
+}
+
+export default CoursesCard
---
+++ src/components/home/hero/Hero.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import "./hero.css";
+import Title from '../../common/title/Title';
+
+
+const Hero = () => {
+  return (
+    <>
+       <section className='hero'>
+        <div className='container'>
+            <div className='row'>
+              <Title subtitle='WELCOME TO ACDAEMIA' title='Best Online Education'/>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore, porro. Cupiditate iure eaque, atque ipsa earum dicta blanditiis sint eum corporis velit eos iusto amet, commodi omnis exercitationem sit modi.</p>
+             <div className="button">
+                <button className="primary-btn">
+                    GET STARTED NOW <i className='fa fa-long-arrow-alt-right'></i>
+                </button>
+                <button >
+                    VIEW COURSE <i className='fa fa-long-arrow-alt-right'></i>
+                </button>
+             </div>
+            </div>
+        </div>
+      </section>
+      <div className='marigin'></div>
+    </>
+  )
+}
+
+export default Hero
